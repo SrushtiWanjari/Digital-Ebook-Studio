efdd3f758f60ff6221a7fefc81b585e161bd9019
SrushtiWanjari <srushtiwan2005@gmail.com>
2025-11-16 09:02:25 +0530

    env added

---
+++ client/src/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+const API = axios.create({
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://digital-ebook-studio-api-a1xb.onrender.com",
+});
+
+export const setAuthToken = (token) => {
+  if (token) API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  else delete API.defaults.headers.common["Authorization"];
+};
+
+export default API;
--- client/src/lib/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-const API = axios.create({ baseURL: 'https://digital-ebook-studio-api-a1xb.onrender.com' });
-
-export const setAuthToken = (token)=> {
-  if (token) API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-  else delete API.defaults.headers.common['Authorization'];
-};
-
-export default API;
